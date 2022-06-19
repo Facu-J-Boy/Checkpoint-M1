488326195dc8a01283b57dc53b661db078f47514
Facu-J-Boy <boyfacundo45@gmail.com>
2022-06-18 21:38:32 -0300

    ejercicio completado

--- 09.js
+++ 09.js
@@ -24,7 +24,16 @@ function filtrar(funcion) {
   // productos.filtrar(function(p) {
   //   return p.price >= 50;
   // }) => [{price: 100, name:'tv'}]
-
+  Array.prototype.filtrar = function (cb) {
+    var nuevoArray = [];
+    for (var i = 0 ; i < this.length ; i++) {
+      var repaso = cb(this[i]);
+      if (repaso === true) {
+      nuevoArray.push(repaso);
+    }
+  }
+  return nuevoArray;
+}
 };
 
 // No modifiques nada debajo de esta linea //
